f381166cdcf05b97e71770f2e12cf2588cfa19f3
Atlas11x <sashat022007@gmail.com>
2022-10-22 21:34:56 +0600

    anti-crush system. v 0.4

--- py_serial_arduino.py
+++ py_serial_arduino.py
@@ -7,39 +7,48 @@ import time
 import os
 
 #Get data from serial port
-port = serial.Serial("/dev/ttyUSB0", 9600)
+try:
+	port = serial.Serial("/dev/ttyUSB0", 9600)
+except:
+	print("Error: can't open port")
 time.sleep(2)
 list_data = []
 
 #Process getting data
 while True:
-	line = port.readline()
-	#print(line)
-	#b'123\r\n'
-	
-#first way:
-	string = line.decode()
-	#print(string)
-	#print(len(string))
-	
-	#list_ = string.split('\r')
-	#print(list_)
-	#print(list_[0])
-#Second way:
-	stripped_string = string.strip()
-	#with \r, \n
-	#print(string)
-	#wihout \r, \n
-	print(stripped_string)
-	list_data.append(stripped_string)
-	if len(list_data) == 22:
-		os.system("/home/oztm/Atlas_/SendSensorData_ver2/SendSensorData/hello_in_file.sh &")
-		break
-	
-	#for math operations
-	num_int = int(stripped_string)
-	#if num_int == 999:
-		#break	
+	try:
+		line = port.readline()
+		#print(line)
+		#b'123\r\n'
+		
+	#first way:
+		string = line.decode()
+		#print(string)
+		#print(len(string))
+		
+		#list_ = string.split('\r')
+		#print(list_)
+		#print(list_[0])
+	#Second way:
+		stripped_string = string.strip()
+		#with \r, \n
+		#print(string)
+		#wihout \r, \n
+		print(stripped_string)
+		list_data.append(stripped_string)
+		if len(list_data) == 22:
+			os.system("./hello_in_file.sh &")
+			break
+	except:
+		print ("Error, retrying...")
+		os.system("rm unit.txt")
+	try:
+		#for math operations
+		num_int = int(stripped_string)
+		#if num_int == 999:
+		#break
+	except:
+		print("Error: Part of code can't run")
 print ("++++")
 
 try:
@@ -54,4 +63,10 @@ except OSError as e:
 	f = open("./unit.txt", 'w')
 	f.close()
 	#exit()
-port.close()
+try:
+	port.close()
+except:
+	print ("Error: can't close port")
+	act = input("Do you want to restart you PC?: y/n:")
+	if act == "y":
+		os.system("reboot")
--- search_file.py
+++ search_file.py
@@ -4,12 +4,13 @@
 import os
 
 while True:
-	check_file = os.path.exists("./probBash.txt")
-
-	if check_file:
-		print("YES: file exists")
-		break
-	else:
-		print("NO: file not exists")
-
+	try:
+		check_file = os.path.exists("./probBash.txt")
 	
+		if check_file:
+			print("YES: file exists")
+			break
+		else:
+			print("NO: file not exists")
+	except:
+		print("Error, retrying...")
